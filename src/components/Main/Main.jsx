8a33254d6198b3a788824eafba7b6a4817c86449
Alexandr Popov <alexep130@yandex.ru>
2024-03-05 23:58:07 +0300

    final 05.03

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,13 +2,11 @@ import AboutProject from "./AboutProject/AboutProject";
 import AboutTechs from "./AboutTechs/AboutTechs";
 import AboutMe from "./AboutMe/AboutMe";
 import PromoProject from "./PromoProject/PromoProject";
-import ScrollToUp from "../ScrollToUp/ScrollToUp";
 
 
 export default function Main() {
     return (
         <main>
-            <ScrollToUp />
             <PromoProject />
             <AboutProject />
             <AboutTechs />
