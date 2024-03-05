f74810b8538202275cd20765d0a0f30961ebce3e
Alexandr Popov <alexep130@yandex.ru>
2024-03-06 00:06:34 +0300

    final 05.03

--- src/components/Main/NavTab/NavTab.jsx
+++ src/components/Main/NavTab/NavTab.jsx
@@ -1,18 +1,5 @@
 function NavTab() {
 
-    // скролл по главной странице (попробовать реализовать на useRef позже)
-    const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]'))
-    for (let anchor of anchors) {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault()
-            const blockID = anchor.getAttribute('href').split('#')[1]
-            document.getElementById(blockID).scrollIntoView({
-                behavior: 'smooth',
-                block: 'start',
-            })
-        })
-    }
-
     return(
         <nav className="navTab">
             <a href="#aboutProject" className="navTab__link link">О проекте</a>
